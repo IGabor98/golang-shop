164383278af3308121a33c4d0d556e73e78ac3dd
ivan.gabor <ivan.gabor@intelliboard.net>
2022-06-25 17:21:12 +0300

    Added routes for create a product and get all products

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"net/http"
+	"shop/models"
+	"shop/repositories"
+)
 
+type Server struct {
+	productRepository repositories.ProductRepository
+}
+
+func main() {
+	productRepository := repositories.ProductRepositoryMock{}
+	server := Server{
+		productRepository: &productRepository,
+	}
+
+	http.HandleFunc("/products", server.createProduct)
+	http.HandleFunc("/products/all", server.GetAll)
+
+	http.ListenAndServe(":8090", nil)
+}
+
+func (s *Server) createProduct(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		product := &models.Product{}
+
+		defer req.Body.Close()
+		if err := json.NewDecoder(req.Body).Decode(product); err != nil {
+			panic(err)
+		}
+
+		createdProduct, err := s.productRepository.Create(*product)
+
+		if err != nil {
+			panic(err)
+		}
+
+		json.NewEncoder(w).Encode(createdProduct)
+	}
+}
+
+func (s *Server) GetAll(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(s.productRepository.GetAll())
 }

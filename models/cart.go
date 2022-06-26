5757665329a909c5b4cacbcd1f66c4a9c8aff3e8
ivan.gabor <ivan.gabor@intelliboard.net>
2022-06-26 18:59:16 +0300

    Created the cart models

---
+++ models/cart.go
@@ -0,0 +1,24 @@
+package models
+
+import "github.com/google/uuid"
+
+type Cart struct {
+	ID       uuid.UUID
+	Products []*Product `json:"products"`
+	Total    float64    `json:"total"`
+}
+
+func (c *Cart) AddProducts(products []*Product) {
+	c.Products = append(c.Products, products...)
+	c.Total = c.calculateTotal()
+}
+
+func (c *Cart) calculateTotal() float64 {
+	result := float64(0)
+
+	for _, product := range c.Products {
+		result += product.Price
+	}
+
+	return result
+}

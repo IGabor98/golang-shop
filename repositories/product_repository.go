7289833dd30bd8f53675004a7087fa288f8c16e5
ivan.gabor <ivan.gabor@intelliboard.net>
2022-06-26 19:12:19 +0300

    Fixed the product repository interface

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -1,11 +1,14 @@
 package repositories
 
-import "shop/models"
+import (
+	"github.com/google/uuid"
+	"shop/models"
+)
 
 type ProductRepository interface {
 	Create(product models.Product) (*models.Product, error)
 	Update(product models.Product) (*models.Product, error)
-	Delete(ID string) error
+	Delete(ID uuid.UUID) error
 	FindByName(name string) []*models.Product
 	GetAll() *[]models.Product
 }
